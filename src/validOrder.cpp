e85c9f24dcf3eb5ac1aa1273675557104e642d33
Chester <chengfang@sjtu.edu.cn>
2019-07-30 20:35:36 +0800

    判断是否合法的出栈序列

--- src/validOrder.cpp
+++ src/validOrder.cpp
@@ -3,9 +3,23 @@
 // 合法的出栈序列：已知从1至n的数字序列，按顺序入栈，每个数字入栈后即可出栈，也可在栈中停留，等待后面的数字入栈出栈后，该数字再出栈，求所给的出栈序列是否合法
 // 例如：3-2-5-4-1是合法的；3-1-2-4-5不合法，因为先让3出栈，则1不可能比2先出栈。
 // 思路：定义一个栈实际尝试能不能按照所给顺序弹出。将所给的出栈序列压入一个队列中。从1开始依次对照队列的头元素决定入栈出栈操作，若最终栈中元素能全部弹出则为合法序列。
+// Sample Input(第一行的数字代表队列元素个数n，接下来有n个数字的行数就是队列元素，接下来的行只有一个数字为0代表元素个数为n的队列结束。下面依次类推，最终一行为0代表输入结束)
+// 5
+// 1 2 3 4 5
+// 5 4 3 2 1
+// 0
+// 6
+// 6 5 4 3 2 1
+// 0
+// 0
+// Sample Output
+// Yes
+// No
 //
+// Yes
 
 #include <iostream>
+//#include <stdio.h>
 #include <queue>
 #include <stack>
 
@@ -27,22 +41,30 @@ bool check_is_valid_order(queue<int> &order) {
 }
 
 int main() {
-    queue<int> order;
-    order.push(3);
-    order.push(2);
-    order.push(5);
-    order.push(4);
-    order.push(1);
+    int n;
+    int train;
+    scanf("%d", &n);
+    while (n) {
+        scanf("%d", &train);
+        while (train) {
+            queue<int> order;
+            order.push(train);
+            for (int i = 1; i < n; ++i) {
+                scanf("%d", &train);
+                order.push(train);
+            }
+            if (check_is_valid_order(order)) {
+                printf("Yes\n");
+            }
+            else {
+                printf("No\n");
+            }
+            scanf("%d",train);
+        }
 
-    queue<int> invalid;
-    invalid.push(3);
-    invalid.push(1);
-    invalid.push(2);
-    invalid.push(4);
-    invalid.push(5);
-
-    cout << check_is_valid_order(order) << endl;
-    cout << check_is_valid_order(invalid) << endl;
+        printf("\n");
+        scanf("%d", &n);
+    }
     return 0;
 }
 
