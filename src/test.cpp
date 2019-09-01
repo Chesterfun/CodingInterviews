c3257d5ec064387822a47be0c1a08b7a7383d044
Chesterfun <chengfang@sjtu.edu.cn>
2019-09-01 20:10:49 +0800

    拼多多机考

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 //
 // Created by shmil on 2019/9/1.
+// 拼多多机试题 给定输入，输出按如下优先级排列：偶数全部在奇数前面；如果同为奇数或者同为偶数，则数值大的在数值小的前面
 //
 
 #include <stdio.h>
@@ -20,10 +21,11 @@ int main() {
         if (x%2 == 0)
         {
             even.push_back(x);
-            //printf("%d\n", x);
+//            printf("even = %d\n", x);
         }
         else {
             odd.push_back(x);
+//            printf("odd = %d\n", x);
         }
         scanf("%d", &x);
         scanf("%c", &y);
@@ -37,34 +39,35 @@ int main() {
     if (y == ';') {
         scanf("%d", &n);
     }
-    //printf("%d", n);
+//    printf("n = %d\n", n);
+
     sort (even.begin(), even.end());
     sort (odd.begin(), odd.end());
-    for (int i = 0; i < even.size(); i++) {
-        printf("%d\n", even[i]);
-    }
-    for (int i = 0; i < odd.size(); i++) {
-        printf("%d\n", odd[i]);
-    }
-    sort (even.begin(), even.end());
-    sort (odd.begin(), odd.end());
+
+//    for (int i = 0; i < even.size(); i++) {
+//        printf("--- even ---%d\n", even[i]);
+//    }
+//    for (int i = 0; i < odd.size(); i++) {
+//        printf("--- odd ---%d\n", odd[i]);
+//    }
+
     int index = 0;
     while (!even.empty() && index < n-1) {
-        printf("%d,", *even.end());
+        printf("%d,", *(even.end()-1));
         even.pop_back();
         index++;
     }
     if (!even.empty() && index == n-1) {
-        printf("%d", *even.end());
+        printf("%d", *(even.end()-1));
         even.pop_back();
-        index++;
         return 0;
     }
 
     while(index < n-1 && !odd.empty()) {
-        printf("%d,", *odd.end());
+        printf("%d,", *(odd.end()-1));
         odd.pop_back();
+        index++;
     }
-    printf("%d", *odd.end());
+    printf("%d", *(odd.end()-1));
     return 0;
 }
