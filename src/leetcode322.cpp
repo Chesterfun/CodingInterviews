1a2ba5197655bdfd66f3b873dbe25aae884413cc
Chesterfun <chengfang@sjtu.edu.cn>
2019-09-18 14:33:51 +0800

    具有最大和的连续子数组

---
+++ src/leetcode322.cpp
@@ -0,0 +1,34 @@
+//
+// Created by shmil on 2019/9/17.
+// 找零钱
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.size(); ++j) {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != -1) {
+                    if (dp[i] == -1 || dp[i] > 1 + dp[i - coins[j]]) {
+                        dp[i] = 1 + dp[i - coins[j]];
+                    }
+                }
+            }
+        }
+        return dp[amount];
+    }
+};
+
+int main(){
+    vector<int> change = {1, 2, 5, 7, 10};
+    int amount = 21;
+    Solution solution;
+    cout << solution.coinChange(change, amount) << endl;
+    return 0;
+}
