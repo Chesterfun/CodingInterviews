340ab42a0c4abbf5e0e77dda8840f61a6ae2f5db
Chesterfun <chengfang@sjtu.edu.cn>
2019-09-23 15:56:28 +0800

    最长回文子串

--- src/leetcode005.cpp
+++ src/leetcode005.cpp
@@ -1,35 +1,32 @@
 //
 // Created by chester on 19-5-7.
+// 最长回文子串，动态规划：dp[i][j]代表区间（i,j)位置的字符串是否为回文串
+// 状态转移方程为dp[i][j] = true, if i = j
+//              dp[i][j] = s[i]==s[j] and (j-i<2 or dp[i-1][j+1]）
 //
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     string longestPalindrome(string s) {
-
-        int n = s.size();
-        if (n<=1) return s;
-        bool f[n][n];
-        fill_n(&f[0][0], n*n, false);
-        int maxLen = 1;
-        int start = 0;
-
-        for (int i = 0; i < n; ++i) {
-            f[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                f[j][i] = (s[j] == s[i]) && (i-j<2 || f[j+1][i-1]);
-                if (f[j][i] && maxLen < i-j+1)
-                {
-                    maxLen = i-j+1;
-                    start = j;
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        size_t start = 0, max_len = 1;
+        for (int j = 0; j < n; ++j) {
+            dp[j][j] = true;
+            for (int i = 0; i < j; ++i) {
+                dp[i][j] = (s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1]));
+                if (dp[i][j] && j - i + 1 > max_len) {
+                    max_len = j - i + 1;
+                    start = i;
                 }
             }
         }
-        return s.substr(start, maxLen);
-
+        return s.substr(start, max_len);
     }
 };
 
